4c8f0d1a9a4e394169d6bc96cf243bb9c58b36c5
LSY <2028238109@qq.com>
2023-07-14 18:08:54 +0800

    From engineer.

--- include/exchanger/header.h
+++ include/exchanger/header.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <ros/package.h>
 #include <sensor_msgs/Image.h>
 #include <std_msgs/String.h>
 #include <algorithm>
@@ -20,8 +20,8 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-
+#include <rm_msgs/ExchangerMsg.h>
+#include <exception>
 class Exchanger
 {
 public:
@@ -33,10 +33,13 @@ public:
     int findMatchPoint(const cv::Point2f &rotate_point ,  const std::vector<cv::Point2i> &inline_points_vec);
     bool checkSequence(const cv::Point2i &p1,const cv::Point2i &p2,const cv::Point2i &p3);
     bool checkArrowSequence(const cv::Point2i &p1,const cv::Point2i &p2,const cv::Point2i &p3);
-    void getPnP(const cv::Mat &rvec,const cv::Mat &tvec,bool shape_signal);
+    void getPnP(const cv::Mat &rvec,const cv::Mat &tvec);
     bool checkArrow(std::vector<std::vector<cv::Point2i>> &hull_vec);
     void getLongLength(int * llength_index,const std::vector<cv::Point2f> &approx_points);
-    float getLineLength(const cv::Point2f & p1,const cv::Point2f & p2);
+    void combinationSolver(const std::vector<cv::Point2i> &inline_points_vec, int start, int k, std::vector<cv::Point2i> &combination_vec, std::vector<std::vector<cv::Point2i>> &combination_save_vec);
+    double getLineLength(const cv::Point2f & p1,const cv::Point2f & p2);
+    void poseNonSensePnP();
+    bool findRectPoints(std::vector<cv::Point2i> &rect_points_vec, const std::vector<cv::Point2i> &inline_points_vec, std::vector<cv::Point2i> &combination_result_vec);
     ros::NodeHandle nh_;
     cv_bridge::CvImagePtr cv_image_;
     ros::Subscriber img_subscriber_;
@@ -46,9 +49,28 @@ public:
     dynamic_reconfigure::Server<exchanger::dynamicConfig> server_;
     dynamic_reconfigure::Server<exchanger::dynamicConfig>::CallbackType callback_;
 
+    bool red_;
     int morph_type_;
     int morph_iterations_;
     int morph_size_;
+    int arrow_area_threshold_;
+    int min_triangle_threshold_;
+
+    int red_lower_hsv_h_;
+    int red_lower_hsv_s_;
+    int red_lower_hsv_v_;
+    int red_upper_hsv_h_;
+    int red_upper_hsv_s_;
+    int red_upper_hsv_v_;
+
+    int blue_lower_hsv_h_;
+    int blue_lower_hsv_s_;
+    int blue_lower_hsv_v_;
+    int blue_upper_hsv_h_;
+    int blue_upper_hsv_s_;
+    int blue_upper_hsv_v_;
+    int max_variance_threshold_;
+
     cv::Mat camera_matrix_;
     cv::Mat distortion_coefficients_;
     std::vector<cv::Point> temp_triangle_hull_;
@@ -65,10 +87,10 @@ public:
     std::vector<cv::Point3f> arrow_right_points2_vec_;
     cv::Mat exchanger_rvec_;
     cv::Mat exchanger_tvec_;
-    cv::Mat arrow_rvec_;
-    cv::Mat arrow_tvec_;
+    cv::Mat arrow_left_rvec_;
+    cv::Mat arrow_left_tvec_;
+    rm_msgs::ExchangerMsg prev_msg_;
     ros::Publisher pnp_publisher_;
-    ros::Publisher flag_publisher_;
     tf2_ros::Buffer tf_buffer_;
     tf::TransformBroadcaster tf_broadcaster_;
 };
